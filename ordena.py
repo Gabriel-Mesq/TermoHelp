0586c05d27c512f963243cd8367bd0048e5d5a94
Gabriel Mesquita <90516048+Gabriel-Mesq@users.noreply.github.com>
2022-07-08 20:24:40 -0300

    Função implementada em 'helper.py'

--- ordena.py
+++
@@ -1,8 +0,0 @@
-with open('palavras_original.txt', 'r') as r:
-
-    palavras = r.readlines()
-
-    with open('palavras.txt', 'w') as w:
-
-        for palavra in palavras:
-            w.write(palavra)
